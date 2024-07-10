54e68dd134dc6abece152f5b8c217805f3e17c02
root <root@DESKTOP-VJ4TJQ2>
2024-07-10 15:57:11 -0400

    created project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>design</title>
+<link rel="stylesheet" href="style.CSS">
+</head>
+<body>
+    <div class="main">
+        <div class= "navabr">
+          <div class="icon">
+            <h2 class="logo">design2</h2>
+          </div>
+        </div>
+    </div>
+
+</body>
+</html>
+
+
+<style>
+main{
+    width: 100px;
+    background: linear-gradient(to top, rgba(0,0,0,0.5)50%), url(https://th.bing.com/th/id/R.0d9102daca2d1e4acac8c3948e17584c?rik=WRIsg%2fl8e2CWtA&riu=http%3a%2f%2fhdqwalls.com%2fwallpapers%2fstill-beach-sd.jpg&ehk=E%2fv0t2ag9Nhvp5dF9fO47Tl3%2f0syHjlTLAV%2bKZOt0zs%3d&risl=1&pid=ImgRaw&r=0)
+    background-position:center
+}
+  
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,8 @@
+<div>
+<h1>
+about page
+
+</h1>
+
+
+</div>
